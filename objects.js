93785089fd4e1c623bccd2b9edd4354477bed243
Alex Holhos <alex.holhos@emanuel.ro>
2018-02-17 17:13:38 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,4 +3,8 @@ function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle;
   return playlist; 
 }
+function removePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist; 
+}
  
